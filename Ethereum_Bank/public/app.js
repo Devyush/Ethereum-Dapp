a7b4ffe38da064790a6bcea629361656f6352c74
Devyush Raturi <devyushraturi@gmail.com>
2022-11-25 09:55:09 +0530

    Dapp

--- Ethereum_Bank/public/app.js
+++ Ethereum_Bank/public/app.js
@@ -1,3 +1,107 @@
+
+
+var ContractABI = [
+	{
+		"inputs": [],
+		"name": "deposite",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdrawfunds",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getbalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getcontractbalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+var ContractAddress = '0x537499C65AED0C7Da8D76a12064Ff4ac0e56ce18';
+
+
+
 document.addEventListener('DOMContentLoaded', async () => {
 
 
@@ -6,6 +110,9 @@ if (typeof window.ethereum !== 'undefined') {
 
     var accounts = await ethereum.request({ method: 'eth_requestAccounts' })
     console.log(accounts);
+
+    web3 = new Web3(window.ethereum);
+    console.log("web3 is loaded", web3);
 
 } else {
     alert('Please install Metamask!')

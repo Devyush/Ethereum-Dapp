14d55461ac18ba86cde50e5ccd1d1e2c4b4e3df0
Devyush Raturi <devyushraturi@gmail.com>
2022-11-25 11:15:35 +0530

    DApp

--- Ethereum_Bank/public/app.js
+++ Ethereum_Bank/public/app.js
@@ -1,5 +1,3 @@
-
-
 var ContractABI = [
 	{
 		"inputs": [],
@@ -100,23 +98,30 @@ var ContractABI = [
 
 var ContractAddress = '0x537499C65AED0C7Da8D76a12064Ff4ac0e56ce18';
 
+var loginbutton = document.getElementById('connect_to_metamask')
+var useraddress = document.getElementById('accountaddress')
+var depositeinput = document.getElementById('depositeeth')
+var depositebutton = document.getElementById('depositebutton')
+var withdrawinput = document.getElementById('withdraweth')
+var withdrawbutton = document.getElementById('withdrawbutton')
+var getbalancebutton = document.getElementById('getbalance')
+var balance = document.getElementById('balance')
 
+var address, web3, myContract
 
 document.addEventListener('DOMContentLoaded', async () => {
+    if (typeof window.ethereum !== 'undefined') {
+        console.log('Metamask is installed!')
 
+        var accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+        console.log(accounts);
 
-if (typeof window.ethereum !== 'undefined') {
-    console.log('Metamask is installed!')
+        web3 = new Web3(window.ethereum);
+        console.log("web3 is loaded", web3);
 
-    var accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-    console.log(accounts);
-
-    web3 = new Web3(window.ethereum);
-    console.log("web3 is loaded", web3);
-
-} else {
-    alert('Please install Metamask!')
-}
-
-
+        myContract = new web3.eth.Contract(ContractABI, ContractAddress);
+        console.log("contract is loaded", myContract);
+    } else {
+        alert('Please install Metamask!')
+    }
 })

8d978e53c811fa174665bb9e022f0c6e79d93ae4
Devyush Raturi <devyushraturi@gmail.com>
2022-11-25 12:44:01 +0530

    DApp

--- Ethereum_Bank/public/app.js
+++ Ethereum_Bank/public/app.js
@@ -116,11 +116,61 @@ document.addEventListener('DOMContentLoaded', async () => {
         var accounts = await ethereum.request({ method: 'eth_requestAccounts' })
         console.log(accounts);
 
-        web3 = new Web3(window.ethereum);
-        console.log("web3 is loaded", web3);
-
         myContract = new web3.eth.Contract(ContractABI, ContractAddress);
         console.log("contract is loaded", myContract);
+
+
+        loginbutton.addEventListener('click', async() => {
+            // ethereum.request({ method: 'eth_requestAccounts' });
+
+            var accounts = await ethereum.request({ method: 'eth_requestAccounts' }); //returns an array of connected accounts
+            address = accounts[0];
+            useraddress.innerText = address;
+
+            useraddress.classList.remove('d-none');
+            loginbutton.classList.add('d-none');
+            console.log(accounts);
+            console.log(accounts[0]);
+        });
+
+        ethereum.on('accountChanged', async() => {
+            // Time to reload your interface with accounts[0]!
+            var accounts = await ethereum.request({ method: 'eth_requestAccounts' }); //returns an array of connected accounts
+            address = accounts[0];
+            useraddress.innerText = address;
+        });
+
+    
+    
+    
+        depositebutton.addEventListener('click', () => {
+
+            console.log(depositeinput.value);
+
+            myContract.methods.deposite().send({from: address, value:depositeinput.value}, function(err, res) {
+                console.log(res);
+            })
+
+        });
+    
+        withdrawbutton.addEventListener('click', () => {
+
+            myContract.methods.withdraw(withdrawinput.value).send({from: address}, function (err, res) {
+                console.log(res);
+            })
+
+        });
+    
+        getbalancebutton.addEventListener('click', () => {
+
+            myContract.methods.getbalance().call({from : address}, function (err,res) {
+                console.log(res);
+                balance.innerText = res;
+            })
+
+        });
+    
+    
     } else {
         alert('Please install Metamask!')
     }
